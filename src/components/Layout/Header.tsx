20c8e81363e0f6ece5e4a47b93aa8c7c35f87ead
Anthoni Campos <anthonithomes@gmail.com>
2024-08-31 10:04:38 -0300

    layout: Ajustando a logo no header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className="absolute w-full z-10 text-white">
-      <div className="bg-blue-500 h-16 px-2 flex sticky items-center">
+      <div className="bg-blue-500 h-16 px-5 flex sticky items-center">
         <p className="text-xl font-bold cursor-pointer">LOGO</p>
       </div>
     </div>
