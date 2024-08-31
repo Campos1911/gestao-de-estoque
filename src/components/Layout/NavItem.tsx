b804d1da4996cf00954b06395c4d97979c42bb6e
Anthoni Campos <anthonithomes@gmail.com>
2024-08-31 10:03:52 -0300

    feature: Criando componente de navegação

---
+++ src/components/Layout/NavItem.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import React from "react";
+import { IconType } from "react-icons";
+
+const NavItem = ({
+  width,
+  NavIcon,
+  link,
+}: {
+  width: string;
+  NavIcon: IconType;
+  link: string;
+}) => {
+  return (
+    <Link
+      href={link}
+      className="flex w-full justify-center items-center gap-3 py-3 bg-blue-500 hover:bg-blue-600 duration-200"
+    >
+      <NavIcon />
+      <p className={`${width === "w-16" ? "hidden" : ""}`}>Início</p>
+    </Link>
+  );
+};
+
+export default NavItem;
--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -1,26 +1,45 @@
 "use client";
 
-import Link from "next/link";
 import React, { useState } from "react";
+import NavItem from "./NavItem";
+import { FaHouseChimney } from "react-icons/fa6";
+import { NavsProps } from "@/@types";
 
 const Sidebar = () => {
   const [width, setWidth] = useState<string>("w-16");
   return (
     <div
-      className={`bg-blue-500 sticky text-white ${width} duration-300 pt-10 flex h-full justify-center`}
+      className={`bg-blue-500 sticky text-white ${width} duration-300 flex flex-col h-full pt-10`}
       onMouseEnter={() => setWidth("w-60")}
       onMouseLeave={() => setWidth("w-16")}
     >
       <div className="flex flex-col w-full">
-        <Link
-          href="/"
-          className="flex w-full justify-center py-3 bg-blue-500 hover:bg-blue-600 duration-200"
-        >
-          Texto
-        </Link>
+        {navs.map((navs, index) => (
+          <NavItem
+            key={index}
+            width={width}
+            NavIcon={navs.icon}
+            link={navs.link}
+          />
+        ))}
       </div>
     </div>
   );
 };
 
 export default Sidebar;
+
+const navs: NavsProps[] = [
+  {
+    icon: FaHouseChimney,
+    link: "/",
+  },
+  {
+    icon: FaHouseChimney,
+    link: "/",
+  },
+  {
+    icon: FaHouseChimney,
+    link: "/",
+  },
+];
