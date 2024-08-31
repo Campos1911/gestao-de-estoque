791e3f0a5ee854cfd3f188ddd0c5ae5f6fb6b011
Anthoni Campos <anthonithomes@gmail.com>
2024-08-31 11:09:58 -0300

    config: Adicionando shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 export default function Home() {
   return (
-    <div className="flex w-full bg-white h-full rounded-tl-3xl p-10"></div>
+    <div className="flex w-full bg-white h-full rounded-tl-3xl p-10">
+      <div>
+        <p className="text-2xl">Seja bem-vindo, Usuário123!</p>
+        <p className="text-lg text-gray-400">
+          Você está olhando o estoque da loja 1!
+        </p>
+      </div>
+    </div>
   );
 }
