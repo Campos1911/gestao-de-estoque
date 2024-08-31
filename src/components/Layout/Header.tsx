7feaa65f6482c658781e9e7828c78ca149a9e301
Anthoni Campos <anthonithomes@gmail.com>
2024-08-31 08:36:10 -0300

    test: Testando o ícone na Sidebar

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="absolute w-full z-10">
-      <div className="bg-blue-500 h-16 px-6 flex sticky items-center">
+    <div className="absolute w-full z-10 text-white">
+      <div className="bg-blue-500 h-16 px-2 flex sticky items-center">
         <p className="text-xl font-bold cursor-pointer">LOGO</p>
       </div>
     </div>
--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import React, { useState } from "react";
 
 const Sidebar = () => {
@@ -7,10 +8,17 @@ const Sidebar = () => {
   return (
     <div
       className={`bg-blue-500 sticky text-white ${width} duration-300 pt-10 flex h-full justify-center`}
-      onMouseEnter={() => setWidth("w-72")}
+      onMouseEnter={() => setWidth("w-60")}
       onMouseLeave={() => setWidth("w-16")}
     >
-      TESTE
+      <div className="flex flex-col w-full">
+        <Link
+          href="/"
+          className="flex w-full justify-center py-3 bg-blue-500 hover:bg-blue-600 duration-200"
+        >
+          Texto
+        </Link>
+      </div>
     </div>
   );
 };
