fe9f8811cc3fcc478b4d3c34045eb7c76181e3d5
Anthoni Campos <anthonithomes@gmail.com>
2024-09-02 11:44:34 -0300

    fix: Trocando cores das bordas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,9 @@ export default function Home() {
     <div className="flex flex-col w-full bg-white h-full rounded-tl-3xl p-6 gap-4">
       <div className="w-full">
         <p className="text-2xl">Seja bem-vindo, Usuário123!</p>
-        <p className="text-lg text-gray-400">
-          Você está olhando o estoque da loja 1!
+        <p className="text-lg text-gray-400 flex gap-1">
+          Você está olhando o estoque da{" "}
+          <p className="text-blue-500">loja 1!</p>
         </p>
       </div>
       <div className="flex w-full py-5 justify-between">
@@ -18,7 +19,7 @@ export default function Home() {
           <CardDashboard key={index} titulo={dados.titulo} dado={dados.dado} />
         ))}
       </div>
-      <div className="border-2 p-5 rounded-lg shadow-lg w-full flex">
+      <div className="border-2 border-blue-500 p-5 rounded-lg shadow-lg w-full flex">
         <div className="flex justify-between w-full">
           {circularInfos.map((dados, index) => (
             <CircularProgress
