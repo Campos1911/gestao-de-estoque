1fc36eff76032ff0d1d13e3e51be753c28e6ceee
Anthoni Campos <anthonithomes@gmail.com>
2024-09-04 21:42:11 -0300

    feature: Dialog de edição criado

--- src/components/Card/CardProdutos.tsx
+++ src/components/Card/CardProdutos.tsx
@@ -2,7 +2,17 @@
 
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
-import { GoPackage } from "react-icons/go";
+
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 const CardProdutos = ({
   nome,
@@ -25,19 +35,47 @@ const CardProdutos = ({
   }, [quantidade]);
 
   return (
-    <Link
-      href={"http://google.com"}
-      target="blank"
-      className="flex w-full items-center justify-between border border-blue-500 rounded-md px-3 py-5 gap-4 shadow-lg cursor-pointer hover:bg-gray-300 duration-200"
-    >
-      <div
-        className={`flex rounded-full ${cor} w-10 h-10 items-center justify-center`}
-      >
-        {quantidade}
-      </div>
-      <p className="sm:text-start text-center">{nome}</p>
-      <p className="text-center">{valor}</p>
-    </Link>
+    <>
+      <Dialog>
+        <DialogTrigger className="outline-none flex w-full items-center justify-between border border-blue-500 rounded-md px-3 py-5 gap-4 shadow-lg cursor-pointer hover:bg-gray-300 duration-200">
+          <div
+            className={`flex rounded-full ${cor} w-10 h-10 items-center justify-center`}
+          >
+            {quantidade}
+          </div>
+          <p className="sm:text-start text-center">{nome}</p>
+          <p className="text-center">{valor}</p>
+        </DialogTrigger>
+        <DialogContent className="w-[90%]">
+          <DialogHeader>
+            <DialogTitle className="overflow-hidden">
+              Registrar mudança no estoque - {nome}
+            </DialogTitle>
+            <DialogDescription>
+              <div className="flex flex-col gap-2 pt-2">
+                <div>
+                  <input
+                    type="number"
+                    className="outline-none border border-blue-500 w-full rounded-md p-1"
+                  />
+                </div>
+                <p>Estoque atual do produto: {quantidade}</p>
+              </div>
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <DialogClose>
+              <button
+                onClick={() => console.log("mudança")}
+                className="bg-blue-500 border border-blue-500 px-10 py-1 rounded-full text-white hover:bg-white hover:text-blue-500 duration-200"
+              >
+                Atualizar
+              </button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
