e904fda5f45d45fee18bdb187c51a4761e15c0f2
Anthoni Campos <anthonithomes@gmail.com>
2024-09-08 18:00:12 -0300

    fix: Corrigindo loading

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="flex flex-col w-full bg-white h-full rounded-tl-3xl p-6 gap-4">
+    <div className="flex flex-col w-full bg-white h-screen sm:h-full rounded-tl-3xl p-6 gap-4">
       <div className="flex items-center justify-center h-full">
         <div className="w-20 h-20 border-4 border-blue-500 border-t-transparent rounded-full animate-spin ease-linear"></div>
       </div>
