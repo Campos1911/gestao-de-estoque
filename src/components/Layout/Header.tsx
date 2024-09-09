ad9dac874e5cf22265a46cd6f311a9f3cb6fa912
Anthoni Campos <anthonithomes@gmail.com>
2024-09-09 18:24:14 -0300

    feature: Menu mobile criado

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -2,6 +2,11 @@
 
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import React, { useState } from "react";
+import { FaHouseChimney } from "react-icons/fa6";
+import { AiFillProduct } from "react-icons/ai";
+import { NavsProps } from "@/@types";
+import { BsFillPeopleFill } from "react-icons/bs";
+import Link from "next/link";
 
 const Header = () => {
   const [visibility, setVisibility] = useState<boolean>(false);
@@ -14,15 +19,39 @@ const Header = () => {
           onClick={() => setVisibility(!visibility)}
         />
       </div>
-      <p
-        className={`bg-red-500 ${
-          visibility ? "flex" : "hidden"
-        } absolute right-0 w-64 p-1`}
-      >
-        Menu
-      </p>
+      {navs.map((nav, index) => (
+        <Link
+          href={nav.link}
+          onClick={() => setVisibility(!visibility)}
+          key={index}
+          className={`bg-blue-500 ${
+            visibility ? "flex" : "hidden"
+          } flex-row items-center gap-3 w-full p-4`}
+        >
+          <nav.icon />
+          {nav.titulo}
+        </Link>
+      ))}
     </div>
   );
 };
 
 export default Header;
+
+const navs: NavsProps[] = [
+  {
+    icon: FaHouseChimney,
+    link: "/",
+    titulo: "Início",
+  },
+  {
+    icon: AiFillProduct,
+    link: "/estoque",
+    titulo: "Estoque",
+  },
+  {
+    icon: BsFillPeopleFill,
+    link: "/pessoas",
+    titulo: "Pessoas",
+  },
+];
